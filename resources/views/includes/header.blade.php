fd95309d4cddd0bd49e2d2bba10ae1b439b823d8
mariocstweb <mariocstmra@gmail.com>
2024-03-11 16:33:43 +0100

    added header

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+  <nav class="navbar p-2">
+    <div class="container-fluid">
+      <a class="navbar-brand">Home</a>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+</header>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,7 +8,7 @@
   @vite('resources/js/app.js')
 </head>
 <body>
-  {{-- @include('includes.header') --}}
+  @include('includes.header')
   <main>
     @yield('main-content')
   </main>
