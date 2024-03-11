718a725931ec050dba2e973b5f105730b0193454
mariocstweb <mariocstmra@gmail.com>
2024-03-11 16:13:54 +0100

    added scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,23 +4,24 @@
 
 
  @section('main-content')
-<section id="movies">
+<section id="movies" class="p-5">
+  <h1 class="text-center">Movies </h1>
   <div class="container p-5">
     <div class="row row-cols-5">
       @forelse($movies as $movie)
-      <div class="col">
+      <div class="col mb-3">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">{{ $movie->title }}</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
-            <p class="card-text">Nationality: {{ $movie->nationality }}</p>
-            <p class="card-text">Date: {{ $movie->date }}</p>
-            <p class="card-text">Vote: {{ $movie->vote }}</p>
+            <h5 class="card-title text-center">{{ $movie->title }}</h5>
+            <h6 class="card-subtitle"><strong>Original name:</strong> {{ $movie->original_title }}</h6>
+            <p class="card-text"><strong>Nationality:</strong> {{ $movie->nationality }}</p>
+            <p class="card-text"><strong>Date:</strong> {{ $movie->date }}</p>
+            <p class="card-text"><strong>Vote:</strong> {{ $movie->vote }}/10</p>
           </div>
         </div>
       </div>
       @empty
-      <div class="col">
+      <div class="col-12">
         <p>No movies found.</p>
       </div>
       @endforelse
